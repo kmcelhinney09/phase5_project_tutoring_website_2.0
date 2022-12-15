c422eceacc78fac1104662e5cbdf73283d1c8f49
Kevin <mr.mcelhinney@gmail.com>
2022-12-14 18:42:11 -0800

    connected tutor to subject

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,6 @@
 class Subject < ApplicationRecord
   has_many :tutored_subjects, dependent: :destroy
+  has_many :tutors, through: :tutored_subjects, foreign_key: :tutor_id
   belongs_to :school
   
   validates :name, :school_id, presence: true
